6f9a4c848165ec5404d6660a52d7bfb0c12c2d02
Jason Blanchard <jas.blanchard@gmail.com>
2020-05-23 13:58:48 -0400

    Updating DNS

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -92,6 +92,26 @@ resource "aws_iam_policy" "kubeconfig_read_write" {
 EOF
 }
 
+resource "aws_iam_policy" "route53_read_write" {
+  name        = "DiRoute53ReadWrite"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+          "Sid" : "AllowPublicHostedZonePermissions",
+          "Effect": "Allow",
+          "Action": [
+              "route53:ChangeResourceRecordSets"
+          ],
+          "Resource": "*"
+      }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "kubeconfig_read_write_to_cluster_instance" {
   role       = aws_iam_role.cluster_instance.name
   policy_arn = aws_iam_policy.kubeconfig_read_write.arn
@@ -102,14 +122,24 @@ resource "aws_iam_role_policy_attachment" "vault_read_to_cluster_instance" {
   policy_arn = data.terraform_remote_state.vault_backend.outputs.vault_read_policy_arn
 }
 
+resource "aws_iam_role_policy_attachment" "route53_read_write_to_cluster_instance" {
+  role       = aws_iam_role.cluster_instance.name
+  policy_arn = aws_iam_policy.route53_read_write.arn
+}
+
 // Instance
 
-resource "aws_instance" "master" {
-  ami                    = "ami-068663a3c619dd892"
-  instance_type          = "t3.small"
-  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.security_group_web_id, data.terraform_remote_state.vpc.outputs.security_group_dmz_id]
+resource "aws_launch_configuration" "node" {
+  name_prefix = "di-k8s-node-"
+  image_id = "ami-068663a3c619dd892"
+  instance_type = "t3.small"
+  security_groups = [data.terraform_remote_state.vpc.outputs.security_group_web_id, data.terraform_remote_state.vpc.outputs.security_group_dmz_id]
   iam_instance_profile   = aws_iam_role.cluster_instance.id
-  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
   user_data              = <<EOT
 #!/bin/bash
 sudo apt-get update
@@ -134,12 +164,16 @@ sed -i "s/#MOREIPS/IP.5 = $PUBLIC_IP\\n#MOREIPS/" /var/snap/microk8s/current/cer
 microk8s stop
 microk8s start
 
-# Create self-signed TLS cert to allow HTTPS connections
+# Create self-signed TLS certs to allow HTTPS connections
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 KEY_FILE=server.key
 CERT_FILE=server.crt
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERT_FILE -subj "/CN=$PUBLIC_HOSTNAME/O=$PUBLIC_HOSTNAME"
-microk8s kubectl create secret tls tls-secret --key $KEY_FILE --cert $CERT_FILE
+microk8s kubectl create secret tls instance-tls-secret --key $KEY_FILE --cert $CERT_FILE
+
+DOMAIN_NAME=di.blanktech.net
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERT_FILE -subj "/CN=$PUBLIC_HOSTNAME/O=$PUBLIC_HOSTNAME"
+microk8s kubectl create secret tls di-blanktech-net-tls-secret --key $KEY_FILE --cert $CERT_FILE
 
 # Initial cluster setup
 git clone https://github.com/jasonblanchard/di-platform
@@ -159,9 +193,19 @@ spec:
   tls:
     - hosts:
       - $PUBLIC_HOSTNAME
-      secretName: tls-secret
+      secretName: instance-tls-secret
+    - hosts:
+      - $DOMAIN_NAME
+      secretName: di-blanktech-net-tls-secret
   rules:
     - host: $PUBLIC_HOSTNAME
+      http:
+        paths:
+        - path: /
+          backend:
+            serviceName: ambassador
+            servicePort: 80
+    - host: $DOMAIN_NAME
       http:
         paths:
         - path: /
@@ -175,6 +219,31 @@ echo "$INGRESS" | microk8s kubectl apply -f -
 
 microk8s kubectl apply -k vault-operator/kustomize
 microk8s kubectl apply -k vault-secrets-webhook/kustomize
+
+CHANGESET="$(cat <<-CHANGESET
+{
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "di.blanktech.net.",
+        "Type": "CNAME",
+        "TTL": 60,
+        "ResourceRecords": [
+          {
+            "Value": "$PUBLIC_HOSTNAME"
+          }
+        ]
+      }
+    }
+  ]
+}
+CHANGESET
+)"
+
+echo $CHANGESET > changeset.json
+
+aws route53 change-resource-record-sets --hosted-zone-id Z1LII87WIHHZ1Y --change-batch file://$(pwd)/changeset.json
 
 # Wait for vault stuff to initialize
 sleep 60
@@ -202,16 +271,27 @@ EOT
   root_block_device {
     volume_size = 32
   }
+}
 
-  tags = {
-    Name = "Di"
+resource "aws_autoscaling_group" "cluster" {
+  name = "di-cluster"
+  launch_configuration = aws_launch_configuration.node.name
+  min_size = 0
+  max_size = 1
+  desired_capacity = 1
+  vpc_zone_identifier = [data.terraform_remote_state.vpc.outputs.public_subnet_id]
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "cluster_public_dns" {
-  value = aws_instance.master.public_dns
-}
-
-output "cluster_public_ip" {
-  value = aws_instance.master.public_ip
-}
+# resource "aws_autoscaling_schedule" "cluster" {
+#   scheduled_action_name = "cluster_schedule"
+#   min_size = 1
+#   desired_capacity = 1
+#   # recurrence = "0 0 * * *"
+#   start_time = "2020-05-23T15:50:00Z"
+#   end_time = "2020-05-23T16:00:00Z"
+#   autoscaling_group_name = aws_autoscaling_group.cluster.name
+# }
